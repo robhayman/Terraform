26a7e00cca4bf1e02bfb88100328c622e04c3df1
Rob Hayman <rob.hayman@microsoft.com>
2020-05-27 12:53:03 +0100

    nsg for MI

---
+++ modules/Managed Instance/Module/nsg_mi.tf
@@ -0,0 +1,207 @@
+# Defining the Resource Group for the Subnet. 
+data "azurerm_resource_group" "nsg" {
+  name = var.nsg_rg
+}
+resource "azurerm_network_security_group" "general_nsg" {
+  name                = var.nsg_name_general
+  location            = "${data.azurerm_resource_group.nsg.location}"
+  resource_group_name = "${data.azurerm_resource_group.nsg.name}"
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Public Endpoint"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "3342"
+    # destination_port_ranges                    = []
+    direction                             = "Inbound"
+    name                                  = "allow_public_endpoint_inbound"
+    priority                              = 1300
+    protocol                              = "Tcp"
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow inbound management traffic"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    # destination_port_range                     = "*"
+    destination_port_ranges               = ["9000", "9003", "1438", "1440", "1452"]
+    direction                             = "Inbound"
+    name                                  = "allow_management_inbound"
+    priority                              = 100
+    protocol                              = "Tcp"
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow inbound traffic inside the subnet"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "*"
+    # destination_port_ranges                    = []
+    direction                             = "Inbound"
+    name                                  = "allow_misubnet_inbound"
+    priority                              = 200
+    protocol                              = "*"
+    source_address_prefix                 = var.mi_Subnet_Prefix
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow On Premis Access"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "*"
+    # destination_port_ranges                    = []
+    direction                             = "Inbound"
+    name                                  = "allow_OnpremisisSQL_MI_Inbound"
+    priority                              = 400
+    protocol                              = "*"
+    source_address_prefix                 = var.on_Prem_Subnet_Inbound_Prefix
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow On Premis Access"
+    destination_address_prefix                 = var.on_Prem_Subnet_Outbound_Prefix
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    # destination_port_range                     = "*"
+    destination_port_ranges               = ["1433", "1434", "50001", "50003", "49411", "53535", "53636", "11000-11999", "14000-14999"]
+    direction                             = "Outbound"
+    name                                  = "allow_OnpremisisSQL_MI_Outbound"
+    priority                              = 400
+    protocol                              = "*"
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow health probe"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "*"
+    # destination_port_ranges                    = []
+    direction                             = "Inbound"
+    name                                  = "allow_health_probe_inbound"
+    priority                              = 300
+    protocol                              = "*"
+    source_address_prefix                 = "AzureLoadBalancer"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow access to data"
+    destination_address_prefixes               = []
+    destination_address_prefix                 = "*"
+    destination_application_security_group_ids = []
+    destination_port_range                     = "1433"
+    # destination_port_ranges                    = []
+    direction                             = "Inbound"
+    name                                  = "allow_tds_inbound"
+    priority                              = 1000
+    protocol                              = "Tcp"
+    source_address_prefix                 = "VirtualNetwork"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Deny"
+    description                                = "Deny all other inbound traffic"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "*"
+    # destination_port_ranges                    = []
+    direction                             = "Inbound"
+    name                                  = "deny_all_inbound"
+    protocol                              = "*"
+    priority                              = 4096
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow outbound management traffic"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    # destination_port_range                     = "*"
+    destination_port_ranges               = ["443", "12000"]
+    direction                             = "Outbound"
+    name                                  = "allow_management_outbound"
+    protocol                              = "Tcp"
+    priority                              = 100
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Allow"
+    description                                = "Allow outbound traffic inside the subnet"
+    destination_address_prefix                 = var.mi_Subnet_Prefix
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "*"
+    # destination_port_ranges                    = []
+    direction                             = "Outbound"
+    name                                  = "allow_misubnet_outbound"
+    protocol                              = "*"
+    priority                              = 200
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+  security_rule {
+    access                                     = "Deny"
+    description                                = "Deny all other outbound traffic"
+    destination_address_prefix                 = "*"
+    destination_address_prefixes               = []
+    destination_application_security_group_ids = []
+    destination_port_range                     = "*"
+    # destination_port_ranges                    = []
+    direction                             = "Outbound"
+    name                                  = "deny_all_outbound"
+    protocol                              = "*"
+    priority                              = 4096
+    source_address_prefix                 = "*"
+    source_address_prefixes               = []
+    source_application_security_group_ids = []
+    source_port_range                     = "*"
+    source_port_ranges                    = []
+  }
+}
