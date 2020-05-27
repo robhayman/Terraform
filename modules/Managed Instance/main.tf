ef3af6cbc7ed6262c50bcb8164f08024245a6c30
Rob Hayman <rob.hayman@microsoft.com>
2020-05-27 14:18:52 +0100

    update to main

--- modules/Managed Instance/main.tf
+++ modules/Managed Instance/main.tf
@@ -1,4 +1,9 @@
 provider "azurerm" {
-  version = ">= 2.0"
+  version = "<= 2.11.0"
   features {}
+
+  subscription_id = "sub_id"
+  tenant_id = "tenant_id"
+  client_id = "client_id"
+  client_secret = "client_secret"
 }
