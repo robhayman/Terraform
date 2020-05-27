589c17de2f3cd95e0bff82eb06ec0fee962632b7
Rob Hayman <rob.hayman@microsoft.com>
2020-05-27 14:22:28 +0100

    added parameters body

--- modules/Managed Instance/Module/managed_instance.tf
+++ modules/Managed Instance/Module/managed_instance.tf
@@ -3,31 +3,66 @@ resource "azurerm_template_deployment" "managed_instance" {
   name                = var.managed_instance_object.name
   resource_group_name = var.resource_group_name
 
-  template_body       = file(local.arm_filename) 
+  template_body       = file("${path.module}/arm_mi_isolated.json")
 
-  parameters = {
-        "managedInstanceName"          = var.managed_instance_object.name
-        "skuName"                      = var.managed_instance_object.sku_name
-        "skuEdition"                   = var.managed_instance_object.sku_tier
-        "location"                     = var.location 
-        "adminUsername"                = var.managed_instance_object.adminUsername
-        "adminPassword"                = var.managed_instance_object.adminPassword
-        "vNetResourceGroup"            = var.managed_instance_object.vNetResourceGroup
-        "vNetName"                     = var.managed_instance_object.vNetName
-        "subnetName"                   = var.managed_instance_object.subnetName
-        "storageSizeInGB"              = var.managed_instance_object.storagesize
-        "vCores"                       = var.managed_instance_object.vcores
-        "licenseType"                  = var.managed_instance_object.licensetype
-        "hardwareFamily"               = var.managed_instance_object.hardwarefamily
-        "dnsZonePartner"               = var.managed_instance_object.dns
-        "collation"                    = var.managed_instance_object.collation
-        "proxyOverride"                = var.managed_instance_object.proxyoveride
-        "publicDataEndpointEnabled"    = var.managed_instance_object.publicDataEndpointEnabled
-        "timezoneId"                   = var.managed_instance_object.timezoneid
+ parameters_body = <<PARAMETERS
+    {
+         "adminUsername": {
+            "value": "${var.managed_instance_object.adminUsername}"
+          },
+          "adminPassword": {
+            "value": "${var.adminPassword}"
+          },
+          "location": {
+            "value": "${var.location}"
+          },
+          "managedInstanceName": {
+            "value": "${var.managed_instance_object.name}"
+          },
+          "skuName": {
+            "value": "${var.managed_instance_object.sku_name}"
+          },
+          "skuEdition": {
+            "value": "${var.managed_instance_object.sku_tier}"
+          },
+          "storageSizeInGB": {
+            "value": ${tonumber(var.managed_instance_object.storagesize)}
+          },
+          "vCores": {
+            "value": ${tonumber(var.managed_instance_object.vcores)}
+          },
+          "licenseType": {
+            "value": "${var.managed_instance_object.licensetype}"
+          },
+          "hardwareFamily": {
+            "value": "${var.managed_instance_object.hardwarefamily}"
+          },
+          "dnsZonePartner": {
+            "value": "${var.managed_instance_object.dns}"
+          },
+          "collation": {
+            "value": "${var.managed_instance_object.collation}"
+          },
+          "proxyOverride": {
+            "value": "${var.managed_instance_object.proxyoveride}"
+          },
+          "timezoneId": {
+            "value": "${var.managed_instance_object.timezoneid}"
+          },
+          "vNetResourceGroup": {
+            "value": "${var.managed_instance_object.vNetResourceGroup}"
+          },
+          "vNetName": {
+            "value": "${var.managed_instance_object.vNetName}"
+          },
+          "subnetName": {
+            "value": "${var.managed_instance_object.subnetName}"
+          },
+          "location": {
+            "value": "${var.location}"
+          }
   }
-
+PARAMETERS
 
   deployment_mode = "Incremental"
-
-
 }
